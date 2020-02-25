94da9dff6d25233a340ea21b22d3bbaef41803c6
Chris Tralie <chris.tralie@gmail.com>
2020-02-25 11:03:19 -0500

    Finishing skeleton code

---
+++ src/Lab5.java
@@ -0,0 +1,33 @@
+
+
+public class Lab5 {
+    /**
+     * 
+     * @param weights An array of weights to use in the average
+     * @param values An array of values to use in the average. Should
+     *               have the same length as weights
+     * @return The weighted average, or 0 if the arrays are empty
+     */
+    public static double getWeightedAverage(double[] weights, double[] values) {
+        assert(weights.length == values.length);
+        double avg = 0.0;
+        
+        // TODO: Fill this in
+        
+        return avg;
+    }
+    
+    /**
+     * 
+     * @param array: An array of numbers
+     * @return The index of the minimum element, or 0
+     *         if the array is empty
+     */
+    public static int getMinIndex(double[] array) {
+        int minIndex = 0;
+        
+        // TODO: Fill this in
+        
+        return minIndex;
+    }
+}
